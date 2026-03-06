ee55b06271b69ae6687e6ae9125397aafa26f0e3
yoojisoo99 <yoojisoo99@naver.com>
2026-03-06 14:26:21 +0900

    main

--- src/main/java/com/basic/myspringboot/MySpringBootApplication.java
+++ src/main/java/com/basic/myspringboot/MySpringBootApplication.java
@@ -1,6 +1,7 @@
 package com.basic.myspringboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
@@ -8,13 +9,19 @@ import org.springframework.context.annotation.Bean;
 public class MySpringBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MySpringBootApplication.class, args);
+
+//        SpringApplication.run(MySpringBootApplication.class, args);
+		SpringApplication application = new SpringApplication(MySpringBootApplication.class);
+		//어플리케이션 타입 설정하기
+		application.setWebApplicationType(WebApplicationType.NONE);
+		application.run(args);
+
 	}
 
 	@Bean
-	public String hello(){
-		System.out.println("=====Spring Bean 입니다. =====");
-		return "Hello World!";
+	public String hello() {
+		System.out.println("=====Spring Bean 입니다. ====");
+		return "Hello Bean";
 	}
 
 }
